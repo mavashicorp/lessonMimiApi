927dc70c60d708db0a3b9eae5428710018f3f2d7
Dmitriy <mavashicorp@gmail.com>
2024-04-27 12:14:40 +0300

    add swagger

--- MinimalASP/MinimalASP/Program.cs
+++ MinimalASP/MinimalASP/Program.cs
@@ -1,12 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//добавитли две строчки выше
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+//добавили две строчки выше
 
 var hotels = new List<Hotel>();
 
