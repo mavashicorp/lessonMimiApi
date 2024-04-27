045de5af9e104b6aa99cc90d1562c83c6c438b1e
Alice <romankoalice@gmail.com>
2024-04-27 11:14:47 +0300

    hotel add Program.cs

--- MinimalASP/MinimalASP/Program.cs
+++ MinimalASP/MinimalASP/Program.cs
@@ -8,27 +8,31 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var hotels = new List<Hotel>();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
+app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
+app.MapPut("/hotels", (Hotel hotel) => {
+    var index = hotels.FindIndex(h => h.Id == hotel.Id);
+    if (index < 0)
+    {
+        throw new Exception("Not found");
+    }
+    hotels[index] = hotel;
+});
+app.MapDelete("/hotels/{id}", (int id) => {
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0) throw new Exception("Not found");
+    hotels.RemoveAt(index);
 });
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+public class Hotel
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
 }
